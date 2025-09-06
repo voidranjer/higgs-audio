9e49065ad46d3931056edd53a3f854c806420e4f
James Yap <contact@jamesyap.org>
2025-09-06 18:58:11 -0400

    chore: multiline script

--- team09/generate.sh
+++ team09/generate.sh
@@ -1,2 +1,11 @@
-git fetch && git reset --hard origin/main && python3 examples/generation.py --scene_prompt examples/scene_prompts/express.txt --transcript examples/transcript/single_speaker/en_express.txt --ref_audio gollum --seed 12345 --out_path generation.wav
+git fetch &&
+  git reset --hard origin/main &&
+  python3 examples/generation.py \
+    --scene_prompt examples/scene_prompts/express.txt \
+    --transcript examples/transcript/single_speaker/en_express.txt \
+    --ref_audio walter \
+    --seed 12345 \
+    --out_path generation.wav
+
+# git fetch && git reset --hard origin/main && python3 examples/generation.py --scene_prompt examples/scene_prompts/express.txt --transcript examples/transcript/single_speaker/en_express.txt --ref_audio gollum --seed 12345 --out_path generation.wav
 # python3 examples/generation.py --scene_prompt examples/scene_prompts/express.txt --transcript examples/transcript/multi_speaker/en_express.txt --ref_audio walter,shrek_donkey,vex,peter,broom_salesman,bigbang_sheldon,en_man --ref_audio_in_system_message --chunk_method speaker --seed 12345 --out_path generation.wav
