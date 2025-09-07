fdfe7e7b1ec45abc2895c1eb927de7198f1de1ad
Raymond Chan <rcwm831@gmail.com>
2025-09-07 06:57:10 +0000

    initialize higgs only once WIP

--- team09/main.py
+++ team09/main.py
@@ -3,6 +3,16 @@ import requests
 import re
 import shutil
 import os
+from generation import main as generate, HiggsAudioModelClient
+
+model_client = HiggsAudioModelClient(
+        model_path="bosonai/higgs-audio-v2-generation-3B-base",
+        audio_tokenizer="bosonai/higgs-audio-v2-tokenizer",
+        device="cuda:0",
+        device_id=0,
+        max_new_tokens=2048,
+        use_static_kv_cache=1,
+    )
 
 messages = []
 
@@ -48,15 +58,28 @@ with open("llm.md", "r") as f:
     initial_prompt = f.read()
 # print(initial_prompt)
 
+# recreate tmp directory
+if os.path.exists("tmp") and os.path.isdir("tmp"):
+    shutil.rmtree("tmp")
+os.makedirs("tmp")
+
 # get script lines
 script = query_qwen(initial_prompt)
 lines = script_to_lines(script)
 
 # generate .wav files
 for i, (speaker, line) in enumerate(lines):
-    with open('tmp/transcript.txt', 'w') as f:
+    with open(f'tmp/{i}.txt', 'w') as f:
        f.write(line) 
-    subprocess.run(f"python3 ../examples/generation.py --scene_prompt ../examples/transcript/express.txt --transcript tmp/transcript.txt --ref_audio {speaker} --chunk_method speaker --seed 12345 --out_path tmp/{i}.wav", shell=True)
+    generate(model_client, 
+             scene_prompt="../examples/transcript/express.txt",
+             transcript='tmp/transcript.txt',
+             ref_audio=speaker,
+             chunk_method='speaker',
+             seed=12345,
+             out_path='tmp/{i}.wav'
+            )
+    # subprocess.run(f"python3 ../examples/generation.py --scene_prompt ../examples/transcript/express.txt --transcript tmp/transcript.txt --ref_audio {speaker} --chunk_method speaker --seed 12345 --out_path tmp/{i}.wav", shell=True)
 
 while True:
     user_response = input()
@@ -67,3 +90,5 @@ while True:
             f.write(line) 
         subprocess.run(f"python3 ../examples/generation.py --scene_prompt ../examples/transcript/express.txt --transcript tmp/transcript.txt --ref_audio {speaker} --chunk_method speaker --seed 12345 --out_path tmp/{i}.wav", shell=True)
 
+if __name__ == '__main__':
+    print('hi')
