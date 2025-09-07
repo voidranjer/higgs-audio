9d384cbb1efc33f43ba00c9964fff038fee92698
James Yap <contact@jamesyap.org>
2025-09-07 07:26:51 -0400

    chore: trying cross language

--- team09/generate.sh
+++ team09/generate.sh
@@ -1,7 +1,7 @@
 python3 examples/generation.py \
   --scene_prompt examples/scene_prompts/express.txt \
   --transcript examples/transcript/single_speaker/en_express.txt \
-  --ref_audio trump \
+  --ref_audio zh_man_sichuan \
   --seed 12345 \
   --out_path generation.wav
 
