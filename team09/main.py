6992966cec83fb391a720cdfd65bb1f8abff36ca
rayc4 <rcwm831@gmail.com>
2025-09-06 20:15:09 -0400

    initial qwen code

---
+++ team09/main.py
@@ -0,0 +1,19 @@
+import subprocess
+import requests
+
+with open("llm.md", "r") as f:
+    initial_prompt = f.read()
+print(initial_prompt)
+
+def query_qwen(prompt):
+    headers = {"Content-Type": "application/json"}
+    data = {
+            "model": "qwen3-30b-a3b-thinking-fp8",
+            "messages": [{"role":"user","content":f"{prompt}"}],
+            "max_tokens": 256
+            }
+    response = requests.post("http://20.66.111.167:31022/v1/chat/completions", headers=headers, json=data)
+    print(response.status_code)
+    print(response.json()['choices'][0]['message']['content'])  # If the response is JSON
+
+query_qwen("what is 4 times 69")
