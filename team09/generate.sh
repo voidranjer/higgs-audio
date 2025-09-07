09c3e60e80cca4ffb9ac3561f4947f77e779651e
James Yap <contact@jamesyap.org>
2025-09-06 23:57:08 -0400

    chore: use steven

--- team09/generate.sh
+++ team09/generate.sh
@@ -1,7 +1,7 @@
 python3 examples/generation.py \
   --scene_prompt examples/scene_prompts/express.txt \
   --transcript examples/transcript/single_speaker/en_express.txt \
-  --ref_audio kim \
+  --ref_audio steven \
   --seed 12345 \
   --out_path generation.wav
 
